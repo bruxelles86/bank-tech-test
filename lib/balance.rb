7663c07bf8a5e524f02b769f724463587db51b43
bruxelles86 <w_gant@yahoo.co.uk>
2017-06-12 15:50:14 +0100

    Updates balance to use floats for money

--- lib/balance.rb
+++ lib/balance.rb
@@ -3,14 +3,14 @@ class Balance
   attr_reader :amount
 
   def initialize
-    @amount = 0
+    @amount = 0.00
   end
 
   def increment(deposit_amount)
-    @amount += deposit_amount
+    @amount += deposit_amount.to_f
   end
 
   def reduce(withdrawal_amount)
-    @amount -= withdrawal_amount
+    @amount -= withdrawal_amount.to_f
   end
 end
