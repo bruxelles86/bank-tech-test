2e2a63e3f0a97c154fb4fe6c9c791b95d26879c3
bruxelles86 <w_gant@yahoo.co.uk>
2017-06-12 12:32:36 +0100

    Adds failing store method to Record class

--- spec/record_spec.rb
+++ spec/record_spec.rb
@@ -8,5 +8,13 @@ describe Record do
     expect(record.transactions.length).to eq(0)
   end
 
+  it 'stores a date, deposit amount, withdrawal amount and current balance for all transactions' do
+    record.store('10/06/2017', 10, 0, 100)
+    expect(record.transactions[0].date).to eq('10/06/2017')
+    expect(record.transactions[0].credit).to eq(10)
+    expect(record.transactions[0].debit).to eq(0)
+    expect(record.transactions[0].balance).to eq(100)
+  end
+
   it 'correctly prints an account statement to STDOUT'
 end
