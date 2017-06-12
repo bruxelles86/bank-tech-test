79f0b722a10b1f83efb121b69cdbcdf372b6e6f3
bruxelles86 <w_gant@yahoo.co.uk>
2017-06-12 11:17:44 +0100

    Adds empty Account class and six pending specs

---
+++ spec/account_spec.rb
@@ -0,0 +1,17 @@
+require 'account'
+
+describe Account do
+  let(:account) { described_class.new }
+
+  it 'has a balance'
+
+  it 'has a record of deposits and withdrawals'
+
+  it 'increments the balance by the correct amount after a deposit' 
+
+  it 'adds deposit details to the record of deposits and withrawals' 
+
+  it 'reduces the balance by the correct amount after a withdrawal' 
+
+  it 'adds withdrawal details to the record of deposits and withdrawals' 
+end
