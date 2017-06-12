c3e5194c4b98622fdbba63662dc3c0d35daff56c
bruxelles86 <w_gant@yahoo.co.uk>
2017-06-12 11:35:30 +0100

    Adds failing test for record initializing with empty transactions array

--- spec/record_spec.rb
+++ spec/record_spec.rb
@@ -3,7 +3,10 @@ require 'record'
 describe Record do
   let(:record) { described_class.new }
 
-  it 'initializes with an empty transactions array'
+  it 'initializes with an empty transactions array' do
+    expect(record.transactions).to be_kind_of(Array)
+    expect(record.transactions.length).to eq(0)
+  end
 
   it 'correctly prints an account statement to STDOUT'
 end
