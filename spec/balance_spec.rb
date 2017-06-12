d7cd6251b485f306d741e5071fffa9cfcbb641a7
bruxelles86 <w_gant@yahoo.co.uk>
2017-06-12 11:30:43 +0100

    Adds failing initial amount test for balance

--- spec/balance_spec.rb
+++ spec/balance_spec.rb
@@ -1,5 +1,9 @@
 require 'balance'
 
 describe Balance do
-  it 'initializes with an amount of zero'
+  let(:balance) { described_class.new }
+
+  it 'initializes with an amount of zero' do
+    expect(balance.amount).to eq(0)
+  end
 end
--- spec/record_spec.rb
+++ spec/record_spec.rb
@@ -1,6 +1,8 @@
 require 'record'
 
 describe Record do
+  let(:record) { described_class.new }
+
   it 'initializes with an empty transactions array'
 
   it 'correctly prints an account statement to STDOUT'
